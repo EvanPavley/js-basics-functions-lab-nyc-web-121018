6cd501c0d54131f50c59c6ceb39feb8bd1f3e741
EvanPavley <github email address>
2019-01-27 16:26:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (travelerSt) {
+  const num_of_blocks = Math.abs(42 - travelerSt);
+  return num_of_blocks;
+}
+
+function distanceFromHqInFeet (travelerSt) {
+  const num_of_feet = distanceFromHqInBlocks(travelerSt) * 264;
+  return num_of_feet;
+}
+
+function distanceTravelledInFeet(st1, st2){
+  const num_of_feet = (Math.abs(st1 - st2)) * 264;
+  return num_of_feet;
+}
+
+function calculatesFarePrice(st1, st2){
+  const dist = distanceTravelledInFeet(st1, st2);
+  let price = 0;
+
+  if (dist < 400) {
+    return price;
+  }else if(dist >= 400 && dist <=2000) {
+    price = (dist - 400) * .02;
+    return price;
+  }else if (dist > 2000 && dist < 2500) {
+    price = 25;
+    return price;
+  }else if (dist > 2500) {
+    return 'cannot travel that far';
+  }
+
+}
